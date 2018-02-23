cbc989660089672b3a7dd466283ea56999724efa
Wagner Bernardes Teixeira <Wagner@DESKTOP-D8G4ILP>
2018-02-23 01:22:15 -0300

    Ajustes

--- todo-app/frontend/src/template/menu.jsx
+++ todo-app/frontend/src/template/menu.jsx
@@ -5,7 +5,7 @@ export default props => (
         <div className='container'>
             <div className='navbar-header'>
                 <a className='navbar-brand' href="#">
-                    <i className='fa fa-calender-check-o'></i>TodoApp
+                    <i className='fa fa-calendar-check-o'></i> TodoApp
                 </a>
             </div>
 
