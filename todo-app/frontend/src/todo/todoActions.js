e5e09f28fb5de0e21241b34d52940aa5953e1a88
Wagner Bernardes Teixeira <Wagner@DESKTOP-D8G4ILP>
2018-02-26 13:31:25 -0300

    Final todo-app com redux

--- todo-app/frontend/src/todo/todoActions.js
+++ todo-app/frontend/src/todo/todoActions.js
@@ -8,20 +8,18 @@ export const changeDescription = event => ({
 })
 
 export const search = () => {
-    const request = axios.get(`${URL}?sort=-createdAt`)
-    return {
-        type: 'TODO_SEARCHED',
-        payload: request
+    return (dispatch, getState) => {
+        const description = getState().todo.description
+        const search = description ? `&description__regex=/${description}/` : ''
+        const request = axios.get(`${URL}?sort=-createdAt${search}`)
+            .then(resp => dispatch({type: 'TODO_SEARCHED', payload: resp.data}))
     }
 }
 
 export const add = description => {
     return dispatch => {
         axios.post(URL, { description })
-            .then(resp => dispatch({
-                type: 'TODO_ADDED',
-                payload: resp.data
-            }))
+            .then(resp => dispatch(clear()))
             .then(resp => dispatch(search()))
     }
 }
@@ -51,3 +49,6 @@ export const remove = todo =>{
 }
 
 
+export const clear = () =>{
+    return  [{type : 'TODO_CLEAR'}, search()]
+} 
