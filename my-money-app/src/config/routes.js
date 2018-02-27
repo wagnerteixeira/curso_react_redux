8d215d966034379eda66d765f84a586bca672544
Wagner Bernardes Teixeira <Wagner@DESKTOP-D8G4ILP>
2018-02-27 01:28:42 -0300

    FInalizado backend

---
+++ my-money-app/src/config/routes.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+module.exports = function(server){
+    //Definir URL base para todas as rotas
+    const router = express.Router()
+    server.use('/api', router)
+
+    const BillingCycle = require('../api/billingCycle/billingCycleService')
+    BillingCycle.register(router, '/billingCycles')
+}
---
+++ my-money-app/src/config/server.js
@@ -0,0 +1,18 @@
+os = require('os')
+const host = os.hostname()
+const port = 3003
+
+const bodyParser = require('body-parser')
+const express = require('express')
+const server = express()
+const allowCors = require('./cors')
+const queryParser = require('express-query-int')
+
+server.use(bodyParser.urlencoded({ extended : true}))
+server.use(bodyParser.json())
+server.use(allowCors)
+server.use(queryParser())
+
+server.listen(port, host, () => console.log(`BAKEND is running on http:\\\\${host}:${port}`))
+
+module.exports = server
