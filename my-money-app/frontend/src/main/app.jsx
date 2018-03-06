7b0bc27f0f0a4a9661c5b3ef3298caddddf63445
Wagner Bernardes Teixeira <Wagner@DESKTOP-D8G4ILP>
2018-03-06 01:30:28 -0300

    Iniciando my money app frontend

---
+++ my-money-app/frontend/src/main/app.jsx
@@ -0,0 +1,11 @@
+import '../common/template/dependencies'
+import React from 'react'
+
+
+import Header from '../common/template/header'
+
+export default props => (
+    <div className='wrapper' >
+        <Header />
+    </div>
+)
